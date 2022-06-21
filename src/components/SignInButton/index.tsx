12a2c851e9805c3c8f13786c458b20d1d98904b5
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-06-20 23:05:11 -0300

    feat: adicionando o componente SignInButton

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = false;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Deivit Eduardo
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#f7326e" />
+      Sign In with Github
+    </button>
+  );
+}
