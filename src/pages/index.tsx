4d3e7460cea35dcf59d2adecf05bc929d2da4f4a
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-06-22 20:41:52 -0300

    feat: adicionando a página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,6 +9,22 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src="/images/avatar.svg" width="334" height="520" alt="Girl Coding" />
+      </main>
     </>
   );
 }
