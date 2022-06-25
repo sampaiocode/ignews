b9654ba0f53841fbe6a95fd6774445bd36827565
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-06-25 16:15:22 -0300

    feat: mudando de SSR para SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { SubscribeButton } from '../components/SubscribeButton';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 import Image from 'next/image';
@@ -42,7 +42,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LEbQ5JJ7IuMKM8EsXvvrtwX');
 
   const product = {
@@ -56,6 +56,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   };
 };
