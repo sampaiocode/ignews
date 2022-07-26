c26f96a56c82dbe8987748db1288babc18931a1e
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-07-26 20:59:27 -0300

    feat: adicionando o componente ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
@@ -9,8 +10,12 @@ export function Header() {
         <div className={styles.headerContent}>
           <Image src="/images/logo.svg" width="110" height="31" alt="ig.news" />
           <nav>
-            <a className={styles.active}>Home</a>
-            <a>Posts</a>
+            <ActiveLink href="/" activeClassName={styles.active}>
+              <a>Home</a>
+            </ActiveLink>
+            <ActiveLink href="/posts" activeClassName={styles.active}>
+              <a>Posts</a>
+            </ActiveLink>
           </nav>
           <SignInButton />
         </div>
