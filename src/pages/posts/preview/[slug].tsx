99a497b7094dbe12754043d74d2648d1591508d6
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-07-31 21:25:25 -0300

    feat: adicionando página de preview do post

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,89 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
+import { getPrismicCLient } from '../../../services/prismic';
+
+import Head from 'next/head';
+import styles from '../post.module.scss';
+import Link from 'next/link';
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [post.slug, router, session]);
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ig.news</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+
+          <div
+            dangerouslySetInnerHTML={{ __html: post.content }}
+            className={`${styles.postContent} ${styles.postPreviewContent}`}
+          />
+
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicCLient();
+
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  };
+
+  return {
+    props: {
+      post
+    }
+  };
+};
