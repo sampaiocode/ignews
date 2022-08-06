22f134a03ec2ab1f0557e7636586483fbd93288b
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-08-06 18:24:46 -0300

    feat: adicionando barra de progresso ao trocar de página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from '../../node_modules/next/app';
 import { Header } from '../components/Header';
+import { Router } from 'next/router';
 import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 
+import nProgress from 'nprogress';
+
 import '../styles/global.scss';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
