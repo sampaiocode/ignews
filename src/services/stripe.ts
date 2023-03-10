bcc97bd04a6340121b915d9db9fdfbf022404f71
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-03-10 20:10:05 -0300

    config: configuring the testing library

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 import { version } from '../../package.json';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
-  apiVersion: '2020-08-27',
+  apiVersion: '2022-11-15',
   appInfo: {
     name: 'Ig.news',
     version
